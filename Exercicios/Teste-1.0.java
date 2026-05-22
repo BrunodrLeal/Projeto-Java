719fd09fbb13cd17fdd812d1384d6b689c6d24d7
BrunodrLeal <bdrleal@gmail.com>
2026-05-21 21:12:10 -0300

    backup pronto

---
+++ Exercicios/Teste-1.0.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Exercicios{
+    public static void main( System.in);{
+
+    Scanner scanner = new Scanner(System.in);
+
+    String l1= "Bruno Leal";
+    String l2 = "Evelin Suellen";
+
+    System.out.println("oi")
+    
+
+    }
+}
